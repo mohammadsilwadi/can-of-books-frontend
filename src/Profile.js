590b644dbd6ee25856089b3d7cf879c70a00e5fb
mohammadsilwadi <mohammadsilwadi1@gmail.com>
2021-08-16 18:48:49 +0300

    part of lab 12

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
 export class Profile extends Component {
+ 
+        componentDidMount =  () => {  
+            if(this.props.auth0.isAuthenticated) {
+               
+              this.props.auth0.getIdTokenClaims()
+              .then(result => {
+                const jwt = result.__raw;
+                const config = {
+                  headers: {"Authorization" : `Bearer ${jwt}`},
+                  method: 'get',
+                  baseURL: process.env.REACT_APP_SERVER_URL,
+                  url: '/test'
+                }
+                axios(config)
+                  .then(axiosResults => console.log(axiosResults.data))
+                  .catch(err => console.error(err));
+              })
+              .catch(err => console.error(err));
+            }
+          }  
     render() {
         const {user,isAuthenticated}=this.props.auth0;
         
